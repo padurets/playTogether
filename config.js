7391eb5c16e1ebfa09f3a5ba334e81a85d14b415
Sergey Padurets <s.padurets@corp.mail.ru>
2018-07-18 14:17:29 +0300

    fix: gitignore

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    steamKey: "XXXXXXXXXXXXXXXXXXXXXXX",
-    redisOptions: undefined, // https://github.com/NodeRedis/node_redis#options-object-properties
-    port: 9001,
-}
-
-module.exports = config
